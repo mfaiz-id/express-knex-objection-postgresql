ccecf54f540ba3b8c4db1bfe60ece95c54cc4636
Faiz <faizal@thegreatsoft.com>
2020-10-06 23:31:51 +0700

    coba knex 2 metode

--- api/controller/UserController.js
+++ api/controller/UserController.js
@@ -1,8 +1,14 @@
 var User = require('../models/User');
+const knex = require('../../db/knex')
 
-exports.index = function(req, res) {
+exports.index = async function(req, res) {
+    // cara 1
     User.query()
-        .then(users => {
-            res.json(users)
-        })
+    .then(users => {
+        res.json(users)
+    })
+
+    // cara 2
+    // const data_user = await knex.raw('select * from users')
+    // res.json({data:data_user.rows})
 };
